8255be6828b7fe83d42cf63313dd02e409d52ca7
thereallucas98 <lucas1998david@gmail.com>
2021-04-18 17:56:16 -0300

    Start Home Page

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -6,23 +6,23 @@ function Home() {
   return (
     <div className="container">
       <header>
-        <div>
-          <span>JobsCalc</span>
-          <span>Você tem 2 horas livres no seu dia</span>
+        <section id="top">
+          <h2>Dashboard</h2>
+          <span id="notification">
+            Você está em 0 projetos!
+          </span>
+          <a id="avatar-profile">
+            <p>David Lucas<span>Meu Perfil</span></p>
+            <img src="https:github.com/thereallucas98.png" />
+          </a>
+        </section>
 
-          <div>
-            <span>Karen</span>
-            <img />
-            <span>Ver perfil</span>
-          </div>
-        </div>
-        <hr />
-        <div></div>
+        <div className="separator-line"></div>
+
+        <section>
+          <h1>Oi</h1>
+        </section>
       </header>
-
-      <main className="container">
-        
-      </main>
     </div>
   );
 }
