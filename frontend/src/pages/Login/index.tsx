76277327024c7c962952d81da8818dd94e6bc89f
thereallucas98 <lucas1998david@gmail.com>
2021-04-22 21:17:24 -0300

    Login Page Created

---
+++ frontend/src/pages/Login/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import './styles.css';
+
+import Photo from '../../assets/img/heloo.svg';
+
+function Login() {
+  return (
+    <div className="container-login">
+      <div />
+      <section>
+        <form action="">
+          <fieldset>
+            <legend>Realize o seu Login</legend>
+            <div className="separator light"></div>
+
+            <div className="input-wrapper">
+              <label htmlFor="name">Email</label>
+              <input type="text" id="email" name="email" />
+            </div>
+            <div className="input-wrapper">
+              <label htmlFor="password">Senha</label>
+              <input
+                placeholder="***********"
+                type="password"
+                id="password"
+                name="password"
+              />
+            </div>
+          </fieldset>
+          <button>
+            Acessar
+            </button>
+          <a href="/create-account">Não possui conta? Crie uma!</a>
+        </form>
+      </section>
+    </div>
+  );
+}
+
+export default Login;
--- frontend/src/routes/app.routes.tsx
+++ frontend/src/routes/app.routes.tsx
@@ -3,13 +3,15 @@ import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
 import AddJob from '../pages/AddJob';
 import Home from '../pages/Home';
+import Login from '../pages/Login';
 import Profile from '../pages/Profile';
 
 const AppRoutes: React.FC = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact component={Home} />
+        <Route path="/" exact component={Login} />
+        <Route path="/dashboard" exact component={Home} />
         <Route path="/add-project" component={AddJob} />
         <Route path="/profile" component={Profile} />
       </Switch>
