01bbc0e204a4e3707b92ea0e71dbb66f1c904b98
thereallucas98 <lucas1998david@gmail.com>
2021-04-26 18:30:14 -0300

    Responsivity - Almost

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'http://138.59.36.63:3333/'
-  baseURL: 'http://localhost:3333'
+  baseURL: 'http://138.59.36.63:3333/'
+  // baseURL: 'http://localhost:3333'
 });
 
 export default api;
